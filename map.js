0336c3eac60272764ad68a1fecec84f18073fd7f
gangerang <ajallchin@gmail.com>
2025-09-18 20:30:47 +1000

    heatmap mvp

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ const map = new maplibregl.Map({
     container: 'map',
     style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
     center: [151.2093, -33.8688],
-    zoom: 12
+    zoom: 10
 });
 
 map.addControl(new maplibregl.GeolocateControl({
@@ -38,17 +38,87 @@ map.on('load', () => {
             });
 
             map.addLayer({
-                'id': 'jacarandas-layer',
-                'type': 'circle',
-                'source': 'jacarandas',
-                'paint': {
-                    'circle-radius': 4,
-                    'circle-color': '#8A2BE2',
-                    'circle-opacity': 0.6
+                id: 'jacarandas-heat',
+                type: 'heatmap',
+                source: 'jacarandas',
+                maxzoom: 14,
+                paint: {
+                    'heatmap-intensity': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        0,
+                        1,
+                        14,
+                        3
+                    ],
+                    'heatmap-color': [
+                        'interpolate',
+                        ['linear'],
+                        ['heatmap-density'],
+                        0,
+                        'rgba(236,222,239,0)',
+                        0.2,
+                        '#d0a9e6',
+                        0.4,
+                        '#b072d4',
+                        0.6,
+                        '#903ac2',
+                        0.8,
+                        '#6c2197'
+                    ],
+                    'heatmap-radius': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        0,
+                        2,
+                        14,
+                        20
+                    ],
+                    'heatmap-opacity': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        13,
+                        1,
+                        14,
+                        0
+                    ]
                 }
             });
 
-            map.on('click', 'jacarandas-layer', (e) => {
+            map.addLayer({
+                id: 'jacarandas-point',
+                type: 'circle',
+                source: 'jacarandas',
+                minzoom: 13,
+                paint: {
+                    'circle-radius': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        13,
+                        2,
+                        16,
+                        5
+                    ],
+                    'circle-color': '#8A2BE2',
+                    'circle-stroke-color': 'white',
+                    'circle-stroke-width': 1,
+                    'circle-opacity': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        13,
+                        0,
+                        14,
+                        1
+                    ]
+                }
+            });
+
+            map.on('click', 'jacarandas-point', (e) => {
                 const coordinates = e.features[0].geometry.coordinates.slice();
                 const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${coordinates[1]},${coordinates[0]}`;
 
@@ -58,13 +128,11 @@ map.on('load', () => {
                     .addTo(map);
             });
 
-            // Change the cursor to a pointer when the mouse is over the jacarandas layer.
-            map.on('mouseenter', 'jacarandas-layer', () => {
+            map.on('mouseenter', 'jacarandas-point', () => {
                 map.getCanvas().style.cursor = 'pointer';
             });
 
-            // Change it back to a pointer when it leaves.
-            map.on('mouseleave', 'jacarandas-layer', () => {
+            map.on('mouseleave', 'jacarandas-point', () => {
                 map.getCanvas().style.cursor = '';
             });
         });
